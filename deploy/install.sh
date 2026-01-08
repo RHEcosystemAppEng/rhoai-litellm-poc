d9ffd06094aee4fc0c46d530cfe332d920fb02c4
Hadar Cohen <81298804+Hadar301@users.noreply.github.com>
2026-01-08 22:32:45 +0200

    Feature/ci for UI (#5)
    * add ci for the streamlit image, activate only when there are changes in the ui directory and only from main branch
    
    * update install script to pull the latest iamge
    
    * change from quay.io/rh-ai-quickstart/litellm-streamlit-ui to quay.io/ecosystem-appeng/litellm-streamlit-ui

--- deploy/install.sh
+++ deploy/install.sh
@@ -43,8 +43,10 @@ echo "[3/3] Deploying LiteLLM with Helm..."
 helm upgrade --install "${RELEASE_NAME}" "${HELM_CHART}" \
     --namespace "${NAMESPACE}" \
     --set ui.enabled=true \
-    --set ui.image.repository=quay.io/rh-ai-quickstart/litellm-streamlit-ui \
+    --set ui.image.repository=quay.io/ecosystem-appeng/litellm-streamlit-ui \
     --set ui.image.tag=latest \
+    --set ui.image.pullPolicy=Always \
+    --force \
     --wait
 
 echo ""
